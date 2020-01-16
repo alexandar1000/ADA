3c12226090370d31a0e65e50156946708fa2f4f0
alexandar1000 <alexandar1000@gmail.com>
2020-01-16 17:10:40 +0000

    Spring Boot and Docker Setup

---
+++ src/main/java/com/ucl/ADA/AdaApplication.java
@@ -0,0 +1,21 @@
+package com.ucl.ADA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class 	AdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+
+}
---
+++ src/test/java/com/ucl/ADA/AdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucl.ADA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
