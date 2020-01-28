56ddf5bf07eae71cf45b4b2cb2581ed3001c96d1
Sebastian V. Burlacu <sebivburlacu@gmail.com>
2020-01-28 10:28:41 +0000

    changes needed for the form from UI to work

--- src/main/java/com/ucl/ADA/repo/RepoController.java
+++ src/main/java/com/ucl/ADA/repo/RepoController.java
@@ -1,24 +1,19 @@
 package com.ucl.ADA.repo;
 
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class RepoController {
 
-    @GetMapping("/analyse")
-    public String analyse(@RequestParam(value = "url") String url,
-                          @RequestParam(value = "branch", defaultValue = "master") String branch) {
-
+    @PostMapping("/download")
+    public void download(@RequestBody Repo repo) {
         RepoService repoService = new RepoService();
         try {
-            repoService.downloadRepository(url, branch);
-            return "success";
+              repoService.downloadRepository(repo);
         } catch (GitAPIException e) {
             e.printStackTrace();
-            return "fail";
         }
     }
 }
