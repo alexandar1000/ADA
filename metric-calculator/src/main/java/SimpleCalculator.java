fa847beb9c119aff3ad2c5114244f1d41d9d153c
Jerry Bian <z.bian2@student.liverpool.ac.uk>
2020-01-28 16:10:05 +0000

    added simple metric calculator modules

---
+++ metric-calculator/src/main/java/SimpleCalculator.java
@@ -0,0 +1,15 @@
+import extractor.SimpleParser;
+
+import java.io.FileNotFoundException;
+
+public class SimpleCalculator {
+
+    static String SRC_DIRECTORY_PATH="parser/source_to_parse/abc";
+
+    static String SRC_FILE_PATH = SRC_DIRECTORY_PATH + "/ServiceCentre.java";
+
+    public static void main(String[] args) throws FileNotFoundException {
+        SimpleParser sp = new SimpleParser();
+        System.out.println(sp.getParsedSourceInJSON(SRC_DIRECTORY_PATH, SRC_FILE_PATH));
+    }
+}
