f1a8d390d986ba4836b78f699aa8450490d968be
sebastianvburlacu <sebivburlacu@gmail.com>
2020-01-28 10:46:34 +0000

    autowired service

--- src/main/java/com/ucl/ADA/repo/RepoController.java
+++ src/main/java/com/ucl/ADA/repo/RepoController.java
@@ -1,15 +1,17 @@
 package com.ucl.ADA.repo;
 
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class RepoController {
+    @Autowired
+    private RepoService repoService;
 
     @PostMapping("/download")
     public void download(@RequestBody Repo repo) {
-        RepoService repoService = new RepoService();
         try {
               repoService.downloadRepository(repo);
         } catch (GitAPIException e) {
