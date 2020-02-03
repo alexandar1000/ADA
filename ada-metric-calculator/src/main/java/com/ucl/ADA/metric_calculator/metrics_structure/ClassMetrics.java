70af412f90deae32fab974610cbefb845dc0c4e7
alexandar1000 <alexandar1000@gmail.com>
2020-02-03 17:29:54 +0000

    Organise metris data representation

--- ada-metric-calculator/src/main/java/com/ucl/ADA/metric_calculator/metrics_structure/ClassMetrics.java
+++ ada-metric-calculator/src/main/java/com/ucl/ADA/metric_calculator/metrics_structure/ClassMetrics.java
@@ -4,31 +4,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Random;
 
 @Getter @Setter @NoArgsConstructor
 public class ClassMetrics {
-    private HashMap<String, MetricValues> invokesInfo = new HashMap<>();
-    private HashMap<String, MetricValues> invokedInfo = new HashMap<>();
 
-    public ClassMetrics randomParsedDataOfClassPlaceholder(String currentClass, ArrayList<String> classNames) {
-        Random random = new Random();
-        for (String className : classNames) {
-            if (!currentClass.equals(className) && random.nextBoolean()) {
-                invokesInfo.put(className, new MetricValues().assignRandomValues());
-            }
-        }
+    private HashMap<String, MetricValues> metricValues = new HashMap<>();
 
-        return this;
-    }
-
-    @Override
-    public String toString() {
-        return "\n\t\tParsedDataOfClassPlaceholder{" +
-                "\n\t\tinvokesInfo=" + invokesInfo +
-                ", \n\t\tinvokedInfo=" + invokedInfo +
-                "\n\t\t}";
-    }
 }
--- ada-metric-calculator/src/main/java/com/ucl/ADA/metric_calculator/metrics_structure/ProjectMetrics.java
+++ ada-metric-calculator/src/main/java/com/ucl/ADA/metric_calculator/metrics_structure/ProjectMetrics.java
@@ -10,23 +10,8 @@ import java.util.HashMap;
 
 @Getter @Setter @NoArgsConstructor
 public class ProjectMetrics {
-    private HashMap<String, ClassMetrics> repositoryValueMap = new HashMap<>();
+
+    private HashMap<String, ClassMetrics> classMetrics = new HashMap<>();
 
     private ArrayList<String> classNames = new ArrayList<>(Arrays.asList("Aaa", "Bbb", "Ccc", "Ddd", "Eee", "Fff"));
-
-    public ProjectMetrics randomParsedDataOfRepoPlaceholder () {
-        for (String className : classNames) {
-            repositoryValueMap.put(className, new ClassMetrics().randomParsedDataOfClassPlaceholder(className, classNames));
-        }
-
-        return this;
-    }
-
-    @Override
-    public String toString() {
-        return "\nParsedDataOfRepoPlaceholder{" +
-                "\n\trepositoryValueMap=" + repositoryValueMap +
-                ",\n\t classNames=" + classNames +
-                "\n}";
-    }
 }
