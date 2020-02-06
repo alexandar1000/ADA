c79399b4f920a9bf0e79c58e9464304f9d1dfde1
alexandar1000 <alexandar1000@gmail.com>
2020-02-06 10:04:49 +0000

    Refactoring

--- ada-parser/src/main/java/com/ucl/ADA/parser/dependence_information/ProjectDependenceTree.java
+++ ada-parser/src/main/java/com/ucl/ADA/parser/dependence_information/ProjectDependenceTree.java
@@ -9,16 +9,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 @Getter @Setter @NoArgsConstructor
 public class ProjectDependenceTree {
     private HashMap<String, ClassDependenceTree> classDependenceTrees = new HashMap<>();
-
-    private ArrayList<String> classNames = new ArrayList<>(Arrays.asList("Aaa", "Bbb", "Ccc", "Ddd", "Eee", "Fff"));
-
 
     public void addModuleDeclaration(String className, PackageDeclarationInformation packageDeclarationInformation) {
         if (this.classDependenceTrees.containsKey(className)) {
