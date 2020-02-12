a4f76c8550f4e81f6b894f4913b1145ed19f7bf9
alexandar1000 <alexandar1000@gmail.com>
2020-02-12 18:18:53 +0000

    Rouing with two pages works now

--- ada-ui/src/app/app-routing.module.ts
+++ ada-ui/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RepoFormComponent } from './repo-form/repo-form.component';
+import { RepositoryGraphComponent } from './repository-graph/repository-graph.component';
 
 const routes: Routes = [
+  { path: 'repository-graph', component: RepositoryGraphComponent},
   { path: 'repo-form', component: RepoFormComponent},
   { path: '', redirectTo: '/repo-form', pathMatch: 'full' },
 ];
