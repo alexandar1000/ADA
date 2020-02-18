dfcfa6c11e5d1e2a0193067def9bbd22308860de
alexandar1000 <alexandar1000@gmail.com>
2020-02-18 14:57:42 +0000

    Refactored dependence_information parameter names to adhere to class names

--- ada-parser/src/main/java/com/ucl/ADA/parser/dependence_information/ProjectStructure.java
+++ ada-parser/src/main/java/com/ucl/ADA/parser/dependence_information/ProjectStructure.java
@@ -50,14 +50,14 @@ public class ProjectStructure {
     /**
      * Adds package declaration for the corresponding declaring class where it is declared.
      * @param declaringClass Class where the package is declared
-     * @param packageDeclarationInformation The package declaration object
+     * @param packageDeclaration The package declaration object
      */
-    public void addPackageDeclaration(String declaringClass, PackageDeclaration packageDeclarationInformation) {
+    public void addPackageDeclaration(String declaringClass, PackageDeclaration packageDeclaration) {
         if (this.classStructures.containsKey(declaringClass)) {
-            this.classStructures.get(declaringClass).setCurrentPackage(packageDeclarationInformation);
+            this.classStructures.get(declaringClass).setCurrentPackage(packageDeclaration);
         } else {
             ClassStructure classStructure = new ClassStructure();
-            classStructure.setCurrentPackage(packageDeclarationInformation);
+            classStructure.setCurrentPackage(packageDeclaration);
             this.classStructures.put(declaringClass, classStructure);
         }
     }
@@ -65,14 +65,14 @@ public class ProjectStructure {
     /**
      * Adds attribute declaration for the corresponding declaring class where it is declared.
      * @param declaringClass Class where the attribute is declared
-     * @param attributeDeclarationInformation The attribute declaration object
+     * @param attributeDeclaration The attribute declaration object
      */
-    public void addAttributeDeclaration(String declaringClass, AttributeDeclaration attributeDeclarationInformation) {
+    public void addAttributeDeclaration(String declaringClass, AttributeDeclaration attributeDeclaration) {
         if (this.classStructures.containsKey(declaringClass)) {
-            this.classStructures.get(declaringClass).addAttributeDeclaration(attributeDeclarationInformation);
+            this.classStructures.get(declaringClass).addAttributeDeclaration(attributeDeclaration);
         } else {
             ClassStructure classStructure = new ClassStructure();
-            classStructure.addAttributeDeclaration(attributeDeclarationInformation);
+            classStructure.addAttributeDeclaration(attributeDeclaration);
             this.classStructures.put(declaringClass, classStructure);
         }
     }
@@ -80,14 +80,14 @@ public class ProjectStructure {
     /**
      * Adds constructor declaration for the corresponding declaring class where it is declared.
      * @param declaringClass Class where the constructor is declared
-     * @param constructorDeclarationInformation The attribute declaration object
+     * @param constructorDeclaration The attribute declaration object
      */
-    public void addConstructorDeclaration(String declaringClass, ConstructorDeclaration constructorDeclarationInformation) {
+    public void addConstructorDeclaration(String declaringClass, ConstructorDeclaration constructorDeclaration) {
         if (this.classStructures.containsKey(declaringClass)) {
-            this.classStructures.get(declaringClass).addConstructorDeclaration(constructorDeclarationInformation);
+            this.classStructures.get(declaringClass).addConstructorDeclaration(constructorDeclaration);
         } else {
             ClassStructure classStructure = new ClassStructure();
-            classStructure.addConstructorDeclaration(constructorDeclarationInformation);
+            classStructure.addConstructorDeclaration(constructorDeclaration);
             this.classStructures.put(declaringClass, classStructure);
         }
     }
@@ -95,14 +95,14 @@ public class ProjectStructure {
     /**
      * Adds method declaration for the corresponding declaring class where it is declared.
      * @param declaringClass Class where the method is declared
-     * @param methodDeclarationInformation The method declaration object
+     * @param methodDeclaration The method declaration object
      */
-    public void addMethodDeclaration(String declaringClass, MethodDeclaration methodDeclarationInformation) {
+    public void addMethodDeclaration(String declaringClass, MethodDeclaration methodDeclaration) {
         if (this.classStructures.containsKey(declaringClass)) {
-            this.classStructures.get(declaringClass).addMethodDeclaration(methodDeclarationInformation);
+            this.classStructures.get(declaringClass).addMethodDeclaration(methodDeclaration);
         } else {
             ClassStructure classStructure = new ClassStructure();
-            classStructure.addMethodDeclaration(methodDeclarationInformation);
+            classStructure.addMethodDeclaration(methodDeclaration);
             this.classStructures.put(declaringClass, classStructure);
         }
     }
@@ -113,22 +113,22 @@ public class ProjectStructure {
      * consuming class where it is consumed.
      * @param consumingClassName class where the package has been invoked from
      * @param declaringClassName class where the package has been declared in
-     * @param packageDeclarationInformation The package declaration object
+     * @param packageInvocation The package declaration object
      */
-    public void addPackageInvocation(String consumingClassName, String declaringClassName, PackageInvocation packageDeclarationInformation) {
+    public void addPackageInvocation(String consumingClassName, String declaringClassName, PackageInvocation packageInvocation) {
         if (this.classStructures.containsKey(consumingClassName)) {
-            this.classStructures.get(consumingClassName).addPackageInvocationElement(declaringClassName, InvocationType.OUTGOING, packageDeclarationInformation);
+            this.classStructures.get(consumingClassName).addPackageInvocationElement(declaringClassName, InvocationType.OUTGOING, packageInvocation);
         } else {
             ClassStructure classStructure = new ClassStructure();
-            classStructure.addPackageInvocationElement(declaringClassName, InvocationType.OUTGOING, packageDeclarationInformation);
+            classStructure.addPackageInvocationElement(declaringClassName, InvocationType.OUTGOING, packageInvocation);
             this.classStructures.put(consumingClassName, classStructure);
         }
 
         if (this.classStructures.containsKey(declaringClassName)) {
-            this.classStructures.get(declaringClassName).addPackageInvocationElement(consumingClassName, InvocationType.INCOMING, packageDeclarationInformation);
+            this.classStructures.get(declaringClassName).addPackageInvocationElement(consumingClassName, InvocationType.INCOMING, packageInvocation);
         } else {
             ClassStructure classStructure = new ClassStructure();
-            classStructure.addPackageInvocationElement(consumingClassName, InvocationType.INCOMING, packageDeclarationInformation);
+            classStructure.addPackageInvocationElement(consumingClassName, InvocationType.INCOMING, packageInvocation);
             this.classStructures.put(declaringClassName, classStructure);
         }
     }
@@ -138,22 +138,22 @@ public class ProjectStructure {
      * consuming class where it is consumed.
      * @param consumingClassName class where the attribute has been invoked from
      * @param declaringClassName class where the attribute has been declared in
-     * @param attributeInvocationInformation The package declaration object
+     * @param attributeInvocation The package declaration object
      */
-    public void addAttributeInvocation(String consumingClassName, String declaringClassName, AttributeInvocation attributeInvocationInformation) {
+    public void addAttributeInvocation(String consumingClassName, String declaringClassName, AttributeInvocation attributeInvocation) {
         if (this.classStructures.containsKey(consumingClassName)) {
-            this.classStructures.get(consumingClassName).addAttributeInvocationElement(declaringClassName, InvocationType.OUTGOING, attributeInvocationInformation);
+            this.classStructures.get(consumingClassName).addAttributeInvocationElement(declaringClassName, InvocationType.OUTGOING, attributeInvocation);
         } else {
             ClassStructure classStructure = new ClassStructure();
-            classStructure.addAttributeInvocationElement(declaringClassName, InvocationType.OUTGOING, attributeInvocationInformation);
+            classStructure.addAttributeInvocationElement(declaringClassName, InvocationType.OUTGOING, attributeInvocation);
             this.classStructures.put(consumingClassName, classStructure);
         }
 
         if (this.classStructures.containsKey(declaringClassName)) {
-            this.classStructures.get(declaringClassName).addAttributeInvocationElement(consumingClassName, InvocationType.INCOMING, attributeInvocationInformation);
+            this.classStructures.get(declaringClassName).addAttributeInvocationElement(consumingClassName, InvocationType.INCOMING, attributeInvocation);
         } else {
             ClassStructure classStructure = new ClassStructure();
-            classStructure.addAttributeInvocationElement(consumingClassName, InvocationType.INCOMING, attributeInvocationInformation);
+            classStructure.addAttributeInvocationElement(consumingClassName, InvocationType.INCOMING, attributeInvocation);
             this.classStructures.put(declaringClassName, classStructure);
         }
     }
@@ -163,22 +163,22 @@ public class ProjectStructure {
      * consuming class where it is consumed.
      * @param consumingClassName class where the constructor has been invoked from
      * @param declaringClassName class where the constructor has been declared in
-     * @param constructorInvocationInformation The package declaration object
+     * @param constructorInvocation The package declaration object
      */
-    public void addConstructorInvocation(String consumingClassName, String declaringClassName, ConstructorInvocation constructorInvocationInformation) {
+    public void addConstructorInvocation(String consumingClassName, String declaringClassName, ConstructorInvocation constructorInvocation) {
         if (this.classStructures.containsKey(consumingClassName)) {
-            this.classStructures.get(consumingClassName).addConstructorInvocationElement(declaringClassName, InvocationType.OUTGOING, constructorInvocationInformation);
+            this.classStructures.get(consumingClassName).addConstructorInvocationElement(declaringClassName, InvocationType.OUTGOING, constructorInvocation);
         } else {
             ClassStructure classStructure = new ClassStructure();
-            classStructure.addConstructorInvocationElement(declaringClassName, InvocationType.OUTGOING, constructorInvocationInformation);
+            classStructure.addConstructorInvocationElement(declaringClassName, InvocationType.OUTGOING, constructorInvocation);
             this.classStructures.put(consumingClassName, classStructure);
         }
 
         if (this.classStructures.containsKey(declaringClassName)) {
-            this.classStructures.get(declaringClassName).addConstructorInvocationElement(consumingClassName, InvocationType.INCOMING, constructorInvocationInformation);
+            this.classStructures.get(declaringClassName).addConstructorInvocationElement(consumingClassName, InvocationType.INCOMING, constructorInvocation);
         } else {
             ClassStructure classStructure = new ClassStructure();
-            classStructure.addConstructorInvocationElement(consumingClassName, InvocationType.INCOMING, constructorInvocationInformation);
+            classStructure.addConstructorInvocationElement(consumingClassName, InvocationType.INCOMING, constructorInvocation);
             this.classStructures.put(declaringClassName, classStructure);
         }
     }
@@ -188,22 +188,22 @@ public class ProjectStructure {
      * consuming class where it is consumed.
      * @param consumingClassName class where the method has been invoked from
      * @param declaringClassName class where the method has been declared in
-     * @param methodInvocationInformation The package declaration object
+     * @param methodInvocation The package declaration object
      */
-    public void addMethodInvocation(String consumingClassName, String declaringClassName, MethodInvocation methodInvocationInformation) {
+    public void addMethodInvocation(String consumingClassName, String declaringClassName, MethodInvocation methodInvocation) {
         if (this.classStructures.containsKey(consumingClassName)) {
-            this.classStructures.get(consumingClassName).addMethodInvocationElement(declaringClassName, InvocationType.OUTGOING, methodInvocationInformation);
+            this.classStructures.get(consumingClassName).addMethodInvocationElement(declaringClassName, InvocationType.OUTGOING, methodInvocation);
         } else {
             ClassStructure classStructure = new ClassStructure();
-            classStructure.addMethodInvocationElement(declaringClassName, InvocationType.OUTGOING, methodInvocationInformation);
+            classStructure.addMethodInvocationElement(declaringClassName, InvocationType.OUTGOING, methodInvocation);
             this.classStructures.put(consumingClassName, classStructure);
         }
 
         if (this.classStructures.containsKey(declaringClassName)) {
-            this.classStructures.get(declaringClassName).addMethodInvocationElement(consumingClassName, InvocationType.INCOMING, methodInvocationInformation);
+            this.classStructures.get(declaringClassName).addMethodInvocationElement(consumingClassName, InvocationType.INCOMING, methodInvocation);
         } else {
             ClassStructure classStructure = new ClassStructure();
-            classStructure.addMethodInvocationElement(consumingClassName, InvocationType.INCOMING, methodInvocationInformation);
+            classStructure.addMethodInvocationElement(consumingClassName, InvocationType.INCOMING, methodInvocation);
             this.classStructures.put(declaringClassName, classStructure);
         }
     }
