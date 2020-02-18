6c47d48a6d431a1eebdc4dafce65c2253db71236
alexandar1000 <alexandar1000@gmail.com>
2020-02-18 13:40:00 +0000

    Refactor MethodDeclaration

--- ada-parser/src/main/java/com/ucl/ADA/parser/dependence_information/declaration_information/MethodDeclaration.java
+++ ada-parser/src/main/java/com/ucl/ADA/parser/dependence_information/declaration_information/MethodDeclaration.java
@@ -1,7 +1,15 @@
 package com.ucl.ADA.parser.dependence_information.declaration_information;
 
-import java.util.ArrayList;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Getter @NoArgsConstructor @Builder
 public class MethodDeclaration extends ElementDeclaration {
     /**
      * The return type of the method.
@@ -11,23 +19,23 @@ public class MethodDeclaration extends ElementDeclaration {
     /**
      * The access modifier assigned to the method.
      */
-    private ModifierType modifierType;
+    private Set<ModifierType> modifierTypes = new HashSet<>();
 
     /**
      * The parameters which the method accepts.
      */
-    private ArrayList<String> parameters = new ArrayList<>();
+    private List<ParameterDeclaration> parameters = new ArrayList<>();
 
     /**
      * The constructor of the method declaration object.
-     * @param modifierType the access modifier associated with the method
+     * @param modifierTypes the access modifier associated with the method
      * @param returnType the method return type
      * @param name name of the method
      * @param parameters the parameters which the method accepts
      */
-    public MethodDeclaration(ModifierType modifierType, String returnType, String name, ArrayList<String> parameters) {
+    public MethodDeclaration(Set<ModifierType> modifierTypes, String returnType, String name, List<ParameterDeclaration> parameters) {
         super(name);
-        this.modifierType = modifierType;
+        this.modifierTypes.addAll(modifierTypes);
         this.returnType = returnType;
         this.parameters.addAll(parameters);
     }
