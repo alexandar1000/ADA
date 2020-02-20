acd81f50d368e07cac7e8e6c7671490450e06685
alexandar1000 <alexandar1000@gmail.com>
2020-02-20 01:09:16 +0000

    Clean ada-metric-calculator module

--- ada-metric-calculator/src/main/java/com/ucl/ADA/metric_calculator/metrics/MetricServices.java
+++ ada-metric-calculator/src/main/java/com/ucl/ADA/metric_calculator/metrics/MetricServices.java
@@ -1,27 +1,8 @@
 package com.ucl.ADA.metric_calculator.metrics;
 
-import com.ucl.ADA.model.project_structure.ProjectStructure;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class MetricServices {
-    @Autowired
-    private MetricRepository metricRepository;
-
-    public ProjectStructure computeAllMetrics(ProjectStructure projectStructure) {
-        projectStructure.computeAllMetrics();
-        return projectStructure;
-    }
-    
-    public List<Metric> getAllMetrics() {
-        return metricRepository.findAll();
-    }
-
-    public Metric getMetric(Long id) {
-        return metricRepository.findById(id).orElse(null);
-    }
 }
 
