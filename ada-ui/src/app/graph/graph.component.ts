f0070a9900f3c5f5a839293f0e51c014a1326fa7
alexandar1000 <alexandar1000@gmail.com>
2020-02-26 22:41:12 +0000

    Display a hardcoded graph

--- ada-ui/src/app/graph/graph.component.ts
+++ ada-ui/src/app/graph/graph.component.ts
@@ -20,6 +20,38 @@ export class GraphComponent implements OnInit {
         tap(_ => console.log('tapped'))
       ).subscribe(data => this.graphData = data);
 
+    // Let's first initialize sigma:
+    var s = new sigma('sigma-container');
+
+    // Then, let's add some data to display:
+    s.graph.addNode({
+      // Main attributes:
+      id: 'n0',
+      label: 'Hello',
+      // Display attributes:
+      x: 0,
+      y: 0,
+      size: 1,
+      color: '#f00'
+    }).addNode({
+      // Main attributes:
+      id: 'n1',
+      label: 'World !',
+      // Display attributes:
+      x: 1,
+      y: 1,
+      size: 1,
+      color: '#00f'
+    }).addEdge({
+      id: 'e0',
+      // Reference extremities:
+      source: 'n0',
+      target: 'n1'
+    });
+
+    // Finally, let's ask our sigma instance to refresh:
+    s.refresh();// Let's first initialize sigma:
+
   }
 
 }
