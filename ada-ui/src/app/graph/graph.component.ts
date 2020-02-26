f9bbf297d0d8e12c14f78eb131335fdc8a824938
alexandar1000 <alexandar1000@gmail.com>
2020-02-26 23:08:32 +0000

    Add all classes to graph, and assign fullyqualified class names as ids

--- ada-ui/src/app/graph/graph.component.ts
+++ ada-ui/src/app/graph/graph.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {AnalyserService} from "../analyser.service";
 import {tap} from "rxjs/operators";
 import { sigma } from 'sigma';
+import {element} from "protractor";
 
 @Component({
   selector: 'app-graph',
@@ -28,30 +29,27 @@ export class GraphComponent implements OnInit {
     var s = new sigma('sigma-container');
 
     // Then, let's add some data to display:
-    s.graph.addNode({
-      // Main attributes:
-      id: 'n0',
-      label: 'Hello',
-      // Display attributes:
-      x: 0,
-      y: 0,
-      size: 1,
-      color: '#f00'
-    }).addNode({
-      // Main attributes:
-      id: 'n1',
-      label: 'World !',
-      // Display attributes:
-      x: 1,
-      y: 1,
-      size: 1,
-      color: '#00f'
-    }).addEdge({
-      id: 'e0',
-      // Reference extremities:
-      source: 'n0',
-      target: 'n1'
-    });
+    let i = 0;
+    for ( let element in this.graphData.classStructures) {
+      console.log(element);
+      let lastIndex = element.lastIndexOf('.');
+      let className = (lastIndex > 0 ? element.substr(lastIndex + 1, element.length - 1) : element);
+
+      s.graph.addNode({
+        id: element,
+        label: className,
+        x: i,
+        y: i++,
+        size: 1,
+        color: '#foo'
+      })
+    }
+    // s.addEdge({
+    //   id: 'e0',
+    //   Reference extremities:
+      // source: 'n0',
+      // target: 'n1'
+    // });
 
     // Finally, let's ask our sigma instance to refresh:
     s.refresh();// Let's first initialize sigma:
