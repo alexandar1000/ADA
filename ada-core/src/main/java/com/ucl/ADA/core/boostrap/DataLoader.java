b49219c30ced0d32eb19ae306b3c0a319e4f5d0b
bzq360 <2733938791@qq.com>
2020-03-01 18:42:14 +0000

    remove unwanted maven dependencies

--- ada-core/src/main/java/com/ucl/ADA/core/boostrap/DataLoader.java
+++ ada-core/src/main/java/com/ucl/ADA/core/boostrap/DataLoader.java
@@ -1,7 +1,5 @@
 package com.ucl.ADA.core.boostrap;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ucl.ADA.model.project_structure.ProjectStructure;
 import com.ucl.ADA.model.project_structure.ProjectStructureService;
 import com.ucl.ADA.parser.ParserServices;
@@ -36,20 +34,5 @@ public class DataLoader implements CommandLineRunner {
         ProjectStructure projectStructureRetrieved = projectStructureService.findById(1L);
 
 
-        ObjectMapper objMapper = new ObjectMapper();
-        String jsonToSave = "[]";
-        String jsonRetrieved = "[]";
-        try {
-            jsonToSave = objMapper.writeValueAsString(projectStructureToSave);
-            jsonRetrieved = objMapper.writeValueAsString(projectStructureRetrieved);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        System.out.println("compare:");
-        System.out.println(jsonToSave == jsonRetrieved);
-        System.out.println("\n\n");
-        System.out.println(jsonToSave);
-        System.out.println("\n\n");
-        System.out.println(jsonRetrieved);
     }
 }
