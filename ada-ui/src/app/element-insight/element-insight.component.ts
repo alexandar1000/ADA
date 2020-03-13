48fb71eedfc2d19e9d9b77bef08f7d132ff2b686
alexandar1000 <alexandar1000@gmail.com>
2020-03-13 14:49:18 +0000

    Add placeholder

--- ada-ui/src/app/element-insight/element-insight.component.ts
+++ ada-ui/src/app/element-insight/element-insight.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ProjectStructure} from "../classes/project-structure";
 
 @Component({
   selector: 'app-element-insight',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ElementInsightComponent implements OnInit {
 
+  @Input() projectStructure: ProjectStructure;
   constructor() { }
 
   ngOnInit() {
