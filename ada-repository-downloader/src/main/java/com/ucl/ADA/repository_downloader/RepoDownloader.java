82ddb423d60b8dbd0f46341639a68128b591f482
naum97 <naum.anteski.16@ucl.ac.uk>
2020-03-20 13:34:13 +0100

    Added method to query latest commit time on a branch

--- ada-repository-downloader/src/main/java/com/ucl/ADA/repository_downloader/RepoDownloader.java
+++ ada-repository-downloader/src/main/java/com/ucl/ADA/repository_downloader/RepoDownloader.java
@@ -4,9 +4,14 @@ import org.eclipse.jgit.api.CreateBranchCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Ref;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -74,14 +79,11 @@ public class RepoDownloader {
 
         GitRepoInfo gitRepoInfo = new GitRepoInfo();
         gitRepoInfo.setUrl(url);
-        String[] data = url.split("/|//");
+
+        String[] data = parseGitUrl(url);
         String owner = data[3];
-        String name;
-        if (data[4].indexOf(".") > 0) {
-            name = data[4].substring(0, data[4].indexOf("."));
-        } else {
-            name = data[4];
-        }
+        String name = data[4];
+
         gitRepoInfo.setRepository(name);
         gitRepoInfo.setOwner(owner);
         gitRepoInfo.setBranch(branch);
@@ -118,5 +120,52 @@ public class RepoDownloader {
             e.printStackTrace();
         }
         return result;
+    }
+
+    /**
+     * Parse the git url to get the name and owner of the repository
+     * @param url url of the git repository
+     * @return a String array containing the name of the owner at [3] index and the repo name at [4] index.
+     */
+    private static String[] parseGitUrl(String url){
+        String[] data = url.split("/|//");
+
+        if (data[4].indexOf(".") > 0)
+            data[4] = data[4].substring(0, data[4].indexOf("."));
+
+        return data;
+    }
+
+    /**
+     * Get the time of the latest (youngest) commit on a given branch of a git repository
+     * @param url url of the git repository
+     * @param branch name of branch
+     * @return the OffsetDateTime of the latest commit time
+     */
+    public static OffsetDateTime getLatestCommitTime(String url, String branch){
+        String[] gitData = parseGitUrl(url);
+        String owner = gitData[3];
+        String name = gitData[4];
+
+        String requestURI = "https://api.github.com/repos/"+owner+"/"+name+"/branches/"+branch;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(requestURI))
+                .build();
+
+        HttpResponse<String> response;
+        try {
+            response = client.send(request,
+                    HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            return null;
+        }
+
+        JSONObject json = new JSONObject(response.body());
+
+        String commitTimeString = json.getJSONObject("commit").getJSONObject("commit").getJSONObject("author").getString("date");
+        DateTimeFormatter fIn = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+
+        return OffsetDateTime.parse(commitTimeString, fIn);
     }
 }
