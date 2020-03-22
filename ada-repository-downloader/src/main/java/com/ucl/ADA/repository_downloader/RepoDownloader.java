82d8811cb8143b2189f1029bacc568cef4e4464e
naum97 <naum.anteski.16@ucl.ac.uk>
2020-03-22 13:49:58 +0100

    Added methods for cloning git repo and filtering source files.

--- ada-repository-downloader/src/main/java/com/ucl/ADA/repository_downloader/RepoDownloader.java
+++ ada-repository-downloader/src/main/java/com/ucl/ADA/repository_downloader/RepoDownloader.java
@@ -1,7 +1,12 @@
 package com.ucl.ADA.repository_downloader;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -12,7 +17,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -24,37 +29,57 @@ import java.util.stream.Stream;
 public class RepoDownloader {
 
     /**
-     * Utility method to list all source files (.java) in a given repository.
-     *
-     * @param directoryPath path to the source directory
-     * @return a list containing all file names ending with .java
+     * Clone a GitHub repository and save it locally in the following hierarchy:
+     * <user.dir>/temp/<owner_name>/<repo_name>/<timestamp>
+     * @param url url of the repository
+     * @param branch branch name
+     * @return the absolute path to the downloaded repository
+     * @throws GitAPIException if the cloning fails
      */
-    private static List<String> getSourceFileNames(String directoryPath) {
-        List<String> result = null;
-        try (Stream<Path> walk = Files.walk(Paths.get(directoryPath))) {
+    public static String downloadRepository(String url, String branch) throws GitAPIException {
 
-            result = walk.map(Path::toString)
-                    .filter(f -> f.endsWith(".java")).collect(Collectors.toList());
+        String rootDirPath = setupRootPath(url);
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result;
+        Git.cloneRepository()
+                .setURI(url)
+                .setDirectory(new File(rootDirPath))
+                .setBranchesToClone(Collections.singletonList("refs/heads/" + branch))
+                .setBranch("refs/heads/" + branch)
+                .call();
+
+        return rootDirPath;
     }
 
     /**
-     * Parse the git url to get the name and owner of the repository
-     *
-     * @param url url of the git repository
-     * @return a String array containing the name of the owner at [3] index and the repo name at [4] index.
+     * Utility method to get the paths to all source files in the downloaded repository.
+     * Filtered in 3 ways:
+     * - only include .java files
+     * - only iterate files which are in a sub-tree of a "src" folder
+     * - exclude files ending with "test.java", regardless of lower/uppercase
+     * @param rootDirectory path to the root directory of the downloaded repository
+     * @return a set of strings containing the paths to all filtered .java files in the repo.
      */
-    private static String[] parseGitUrl(String url) {
-        String[] data = url.split("/|//");
+    public static Set<String> getSourceFilePaths(String rootDirectory) {
 
-        if (data[4].indexOf(".") > 0)
-            data[4] = data[4].substring(0, data[4].indexOf("."));
+        File dir = new File(rootDirectory);
+        List<String> sourceDirectories = getSourceDirectories(dir);
 
-        return data;
+        Set<String> sourceFiles = new HashSet<>();
+
+        for (String srcDir : sourceDirectories) {
+
+            try (Stream<Path> walk = Files.walk(Paths.get(srcDir))) {
+                sourceFiles = walk.map(Path::toString)
+                        .filter(f -> f.endsWith(".java") && !f.toLowerCase().endsWith("test.java"))
+                        .map(s -> s.toLowerCase().substring(s.indexOf("src")))
+                        .collect(Collectors.toSet());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sourceFiles;
     }
 
     /**
@@ -89,5 +114,50 @@ public class RepoDownloader {
         DateTimeFormatter fIn = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
         return OffsetDateTime.parse(commitTimeString, fIn);
+    }
+
+    private static String setupRootPath(String url) {
+
+        OffsetDateTime offsetDateTime = OffsetDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss");
+        String timeStamp = offsetDateTime.format(formatter);
+
+        String[] data = parseGitUrl(url);
+        String owner = data[3];
+        String repoName = data[4];
+
+
+        return System.getProperty("user.dir")+"/temp/"+owner+"/"+repoName+"/"+timeStamp;
+    }
+
+    private static List<String> getSourceDirectories(File rootDirectory) {
+        List<String> sourceDirectories = new ArrayList<>();
+
+        Collection<File> files = FileUtils.listFilesAndDirs(rootDirectory,
+                TrueFileFilter.TRUE,
+                TrueFileFilter.TRUE);
+
+        for (File file : files) {
+            if(!file.isDirectory()) continue;
+            if (file.getName().toLowerCase().equals("src"))
+                sourceDirectories.add(file.getPath());
+        }
+
+        return sourceDirectories;
+    }
+
+    /**
+     * Parse the git url to get the name and owner of the repository
+     *
+     * @param url url of the git repository
+     * @return a String array containing the name of the owner at [3] index and the repo name at [4] index.
+     */
+    private static String[] parseGitUrl(String url) {
+        String[] data = url.split("/|//");
+
+        if (data[4].indexOf(".") > 0)
+            data[4] = data[4].substring(0, data[4].indexOf("."));
+
+        return data;
     }
 }
