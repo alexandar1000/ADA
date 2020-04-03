3f6b08b13bd41f919f1a730c2277153ad1a940b7
naum97 <naum.anteski.16@ucl.ac.uk>
2020-04-03 11:53:11 +0200

    added some more Downloader tests

--- ada-repository-downloader/src/test/java/com/ucl/ADA/repository_downloader/RepoDownloaderTest.java
+++ ada-repository-downloader/src/test/java/com/ucl/ADA/repository_downloader/RepoDownloaderTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.nio.file.Paths;
+import java.time.OffsetDateTime;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -65,4 +68,44 @@ public class RepoDownloaderTest {
         });
 
     }
+
+    @Test
+    void testGetLatestCommitTime(){
+        String branch = "master";
+        String url = "https://github.com/alexandar1000/ADA-test-simple-JAVA-project-0.git";
+        OffsetDateTime dateTime = RepoDownloader.getLatestCommitTime(url, branch);
+
+        assertNotNull(dateTime);
+    }
+
+    @Test
+    void testGetLatestCommitTimeWithWrongBranch(){
+        String branch = "non-existing-branch";
+        String url = "https://github.com/alexandar1000/ADA-test-simple-JAVA-project-0.git";
+
+        OffsetDateTime dateTime = RepoDownloader.getLatestCommitTime(url, branch);
+        assertNull(dateTime);
+    }
+
+    @Test
+    void testParseGitUrl(){
+        String url = "https://github.com/alexandar1000/ADA-test-simple-JAVA-project-0.git";
+        String[] arr = RepoDownloader.parseGitUrl(url);
+
+        assertEquals("alexandar1000", arr[3]);
+        assertEquals("ADA-test-simple-JAVA-project-0", arr[4]);
+    }
+
+    @Test
+    void testGetSourceFilesPath() throws GitAPIException {
+        String branch = "master";
+        String url = "https://github.com/alexandar1000/ADA-test-simple-JAVA-project-0.git";
+
+        String rootPath = RepoDownloader.downloadRepository(url, branch);
+        assertTrue(Paths.get(rootPath).toFile().isDirectory());
+
+        Set<String> paths = RepoDownloader.getSourceFilePaths(rootPath);
+        assertFalse(paths.isEmpty());
+    }
+
 }
