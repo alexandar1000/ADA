be4c1ae58ada5cbfe24995be777abfddb5864102
bzq360 <2733938791@qq.com>
2020-04-03 01:35:20 +0100

    add main controller

---
+++ ada-core/src/main/java/com/ucl/ADA/core/repository_analyser/RepositoryAnalyserController.java
@@ -0,0 +1,23 @@
+package com.ucl.ADA.core.repository_analyser;
+
+import com.ucl.ADA.model.snapshot.Snapshot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/analyser")
+public class RepositoryAnalyserController {
+    @Autowired
+    RepositoryAnalyserServices repositoryAnalyserServices;
+
+    /**
+     * A top level endpoint which will analyse the entire repository branch which is provided, at the current time
+     * point. Currently awaiting other modules to be developed.
+     * @return Currently only a string, but in the future something which links to the resulting data.
+     */
+    @CrossOrigin(origins = "http://localhost:4200")
+    @PostMapping(produces = "application/json")
+    public Snapshot analyseRepository(@RequestParam(value = "url", defaultValue = "") String url, @RequestParam(value = "branch", defaultValue = "") String branchName) {
+        return repositoryAnalyserServices.analyseRepositoryService(url, branchName);
+    }
+}
